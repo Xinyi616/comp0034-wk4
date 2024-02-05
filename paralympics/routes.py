4b7b738cd1ae08cec2cfe660b4fea86ef93626c8
Xinyi616 <Xinyiwang020616@gmail.com>
2024-02-05 17:45:30 +0000

    complete week 4

--- paralympics/routes.py
+++ paralympics/routes.py
@@ -1,6 +1,6 @@
-from flask import current_app as app, request, make_response
+from flask import current_app as app, request, make_response, abort, jsonify
 from marshmallow import ValidationError
-
+from sqlalchemy import exc
 from paralympics import db
 from paralympics.models import Region, Event
 from paralympics.schemas import RegionSchema, EventSchema
@@ -10,6 +10,32 @@ regions_schema = RegionSchema(many=True)
 region_schema = RegionSchema()
 events_schema = EventSchema(many=True)
 event_schema = EventSchema()
+
+@app.errorhandler(ValidationError)
+def register_validation_error(error):
+    """ Error handler for marshmallow schema validation errors.
+
+    Args:
+        error (ValidationError): Marshmallow error.
+
+    Returns:
+        HTTP response with the validation error message and the 400 status code
+    """
+    response = error.messages
+    return response, 400
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    """ Error handler for 404.
+
+        Args:
+            HTTP 404 error
+
+        Returns:
+            JSON response with the validation error message and the 404 status code
+        """
+    return jsonify(error=str(e)), 404
 
 
 @app.get("/regions")
@@ -30,17 +56,21 @@ def get_regions():
 def get_region(code):
     """ Returns one region in JSON.
 
-    :param code: The NOC code of the region to return
-    :param type code: str
-    :returns: JSON
-    """
-    # Query structure shown at https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/queries/#select
-    region = db.session.execute(db.select(Region).filter_by(NOC=code)).scalar_one()
-    # Dump the data using the Marshmallow region schema; '.dump()' returns JSON.
-    result = region_schema.dump(region)
-    # Return the data in the HTTP response
-    return result
+    Returns 404 if the region code is not found in the database.
 
+    Args:
+        code (str): The 3 digit NOC code of the region to be searched for
+    Returns: 
+        JSON for the region if found otherwise 404
+    """
+    # Try to find the region, if it is not found, catch the error and return 404
+    try:
+        region = db.session.execute(db.select(Region).filter_by(NOC=code)).scalar_one()
+        result = region_schema.dump(region)
+        return result
+    except exc.NoResultFound as e:
+        # See https://flask.palletsprojects.com/en/2.3.x/errorhandling/#returning-api-errors-as-json
+        abort(404, description="Region not found")
 
 @app.get("/events")
 def get_events():
